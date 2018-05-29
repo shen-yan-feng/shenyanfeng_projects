5a8ff4aa7ef19d2c1d87801841f1d822ae0befe5
shen-yan-feng <32232416+shen-yan-feng@users.noreply.github.com>
2018-05-30 07:53:25 +0800

    just a test

---
+++ eclipse-workspace/test01/src/test01/Shell01.java
@@ -0,0 +1,52 @@
+package test01;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class Shell01 extends Shell {
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		try {
+			Display display = Display.getDefault();
+			Shell01 shell = new Shell01(display);
+			shell.open();
+			shell.layout();
+			while (!shell.isDisposed()) {
+				if (!display.readAndDispatch()) {
+					display.sleep();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the shell.
+	 * @param display
+	 */
+	public Shell01(Display display) {
+		super(display, SWT.SHELL_TRIM);
+		createContents();
+	}
+
+	/**
+	 * Create contents of the shell.
+	 */
+	protected void createContents() {
+		setText("SWT Application");
+		setSize(450, 300);
+
+	}
+
+	@Override
+	protected void checkSubclass() {
+		// Disable the check that prevents subclassing of SWT components
+	}
+
+}
