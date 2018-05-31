e24eeba19291b20e6764457b899b076487c365d3
shen-yan-feng <32232416+shen-yan-feng@users.noreply.github.com>
2018-05-31 22:23:45 +0800

    XUL LEARNING

--- workspace/XulTest/src/org/peter/Test01.java
+++ workspace/XulTest/src/org/peter/Test01.java
@@ -4,7 +4,8 @@ import org.pentaho.ui.xul.swt.SwtXulLoader;
 import org.pentaho.ui.xul.swing.SwingXulLoader;
 import org.pentaho.ui.xul.XulDomContainer;
 import org.pentaho.ui.xul.XulException;
-
+import org.pentaho.ui.xul.swing.SwingXulRunner;
+import org.pentaho.ui.xul.XulRunner;
 
 
 public class Test01 {
@@ -14,7 +15,15 @@ public class Test01 {
 		
 
 			try {
-				XulDomContainer xulDomContainer = new SwingXulLoader().loadXul("E:\\GitHub\\shenyanfeng_projects\\workspace\\XulTest\\src\\xul\\preview_rows.xul");
+				XulDomContainer container = new SwingXulLoader().loadXul("E:\\GitHub\\shenyanfeng_projects\\workspace\\XulTest\\src\\xul\\preview_rows.xul");
+				//MainController controller = new MainController();
+				//container.addEventHandler(controller);
+				final XulRunner runner = new SwingXulRunner();
+				runner.addContainer(container);
+				runner.initialize();
+				runner.start();
+				
+				
 			} catch (IllegalArgumentException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
